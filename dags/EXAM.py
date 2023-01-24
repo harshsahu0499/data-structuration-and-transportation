f89a9981180f0917d188f55c3a4bc54f87fee3c3
Harsh Sahu <harshsahu0499@gmail.com>
2023-01-24 18:53:43 +0100

    exam main added - harsh and shiyama

---
+++ dags/EXAM.py
@@ -0,0 +1,37 @@
+# HARSH SAHU
+# SHIYAMA Suntharalingam
+
+
+import json
+from dataclasses import dataclass
+from datetime import datetime
+import requests
+
+from airflow.decorators import dag, task
+
+@dag(
+    schedule=None,
+    start_date=datetime(2023, 1, 11),
+    catchup=False
+)
+def EXAM():
+    @task
+    def read_flights(link: str):
+        r = requests.get(link)
+        flights = r.json()
+        flightsString = json.dumps(flights)
+        print(flightsString)
+        return flightsString
+
+    @task
+    def write_toJSON(flights):
+        flights_JSON = json.loads(flights)
+        with open('./dags/EXAM_OUTPUT_flights.json', "w") as f:
+            json.dump(flights_JSON,f)
+
+
+    flights = read_flights("https://opensky-network.org/api/flights/departure?airport=LFPG&begin=1669852800&end=1669939200")
+    write_toJSON(flights)
+
+
+_ = EXAM()
